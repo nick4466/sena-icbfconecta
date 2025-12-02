4ff036bf765905f41ace7dce535c93b3894b2eca
nick4466 <stivnieto14@gmail.com>
2025-12-02 00:14:29 -0500

    cambio allowed_host

--- icbfconecta/settings.py
+++ icbfconecta/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-4+5-$@(pm((98+wh48-#4%rcye^!ug3_(84qtt%p*ubaoua_(o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
